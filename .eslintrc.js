b62c15f7a45aaf90cfb657f568644e98b8c6a166
Pythux <pythux@nowhere.org>
2019-10-21 13:21:52 +0200

    init
    yarn create nuxt-app <project-name>

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended'
+  ],
+  rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+      "indent": [
+          1,
+          4
+      ],
+      "comma-dangle": [
+          "error",
+          "always-multiline"
+      ],
+      "space-before-function-paren": ["error", {
+          "anonymous": "never",
+          "named": "never",
+          "asyncArrow": "always",
+      }],
+      "vue/max-attributes-per-line": [
+          "error",
+          {
+              "singleline": 8,
+              "multiline": {
+                  "max": 1,
+                  "allowFirstLine": false
+              }
+          }
+      ]
+  },
+}
