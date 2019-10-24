6dc78d99307154e2a284eeae552c0c5efab72bc8
Pythux <pythux@nowhere.org>
2019-10-24 11:28:53 +0200

    mode spa on config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
-    mode: 'universal',
+    mode: 'spa',
     env: { // environement variables
         baseUrl: process.env.baseUrl || 'https://my.firebaseio.com',
     },
