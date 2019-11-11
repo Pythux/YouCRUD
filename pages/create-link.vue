05d8aa2e1ea8d2633f1612363a9d9e7f501e484a
Pythux <pythux@nowhere.org>
2019-11-11 19:14:15 +0100

    handles new tags with on v-autocomplete

--- pages/create-link.vue
+++ pages/create-link.vue
@@ -21,16 +21,18 @@
             <v-text-field v-model="url" label="URL:" />
             <v-autocomplete
               v-model="tags"
-              :value="tagCurrentValue"
               :items="existingTags"
-              :filter="filter"
+              :search-input="currentTag"
+              :no-data-text="`press enter to add the tag: ${currentTag}`"
               outlined
               dense
               chips
               small-chips
               label="Tags"
               multiple
-              @keyup.enter="addNewTag"
+              autocomplete="off"
+              @update:search-input="updateCurrentTag"
+              @keydown.enter.native.prevent="addNewTag"
             />
           </v-card-text>
           <v-card-actions>
@@ -49,41 +51,41 @@ export default {
     data() {
         return {
             name: null,
-            url: null,
+            url: '',
             tags: [],
-            existingTags: [],
-            tagCurrentValue: null,
-            ytId: null,
+            existingTags: ['a', 'b'],
+            currentTag: '',
         }
     },
     computed: {
         ytLink() {
             return 'http://www.youtube.com/embed/' + this.ytId + '?autoplay=1'
         },
-    },
-    methods: {
-        filter(item, queryText, itemText) {
-            this.tagCurrentValue = queryText
-            return itemText.toLocaleLowerCase().includes(queryText.toLocaleLowerCase())
-        },
-        addNewTag() {
-            console.log(this.tagCurrentValue)
-            if (!this.existingTags.includes(this.tagCurrentValue)) {
-                this.existingTags.push(this.tagCurrentValue)
-            }
-        },
-        submit() {
-            console.log('hey')
-            console.log(this.url)
+        ytId() {
+            console.log('computed!')
             if (this.url.startsWith('https://www.youtube.com/watch')) {
                 const url = new URL(this.url)
-                this.ytId = url.searchParams.get('v')
-                console.log(url.searchParams)
+                return url.searchParams.get('v')
             }
+            return null
+        },
+    },
+    methods: {
+        updateCurrentTag(tag) {
+            this.currentTag = tag
+        },
+        addNewTag() {
+            console.log(this.currentTag)
+            this.existingTags.push(this.currentTag)
+            this.tags.push(this.currentTag)
+            this.currentTag = ''
+        },
+        submit() {
+            console.log('submit:')
+            console.log(this.ytId)
+            // https://you-crud.firebaseio.com/users/<user_id>.json?auth=<idToken>
+            // this.$http()
         },
     },
 }
 </script>
-
-<style lang="css" scoped>
-</style>
