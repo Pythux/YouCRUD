a31035f7988f7bd70d5fb2619ee7cb2516879ac3
Pythux <pythux@nowhere.org>
2019-12-09 17:40:47 +0100

    add new tags

--- components/autocompleteTags.vue
+++ components/autocompleteTags.vue
@@ -1,10 +1,10 @@
 <template lang="html">
   <v-row>
-    <!-- <v-col cols="2">
+    <v-col cols="2">
       <v-btn color="green" @click="newTag = !newTag">
         <plus-circle-icon />
       </v-btn>
-    </v-col> -->
+    </v-col>
     <v-col cols="10">
       <v-autocomplete
         v-if="!newTag"
@@ -23,7 +23,7 @@
         @update:search-input="updateCurrentTag"
         @keydown.enter.native.prevent="currentTag = ''"
       />
-      <v-text-field v-else />
+      <v-text-field v-else v-model="tagToAdd" label="new Tag:" @keydown.enter.prevent="addNewTag()" />
     </v-col>
   </v-row>
 </template>
@@ -38,6 +38,7 @@ export default {
             currentTag: '',
             addedTags: [],
             newTag: false,
+            tagToAdd: '',
         }
     },
     computed: {
@@ -47,14 +48,21 @@ export default {
     },
     methods: {
         addNewTag() {
-            if (!this.existingTags.includes(this.currentTag)) {
-                this.addedTags.push(this.currentTag)
-                this.value.push(this.currentTag)
-                this.currentTag = ''
-            } else {
-                this.currentTag = ''
-            }
+            console.log('yo')
+            this.addedTags.push(this.tagToAdd)
+            this.value.push(this.tagToAdd)
+            this.tagToAdd = ''
+            this.newTag = false
         },
+        // addNewTag() {
+        //     if (!this.existingTags.includes(this.currentTag)) {
+        //         this.addedTags.push(this.currentTag)
+        //         this.value.push(this.currentTag)
+        //         this.currentTag = ''
+        //     } else {
+        //         this.currentTag = ''
+        //     }
+        // },
         updateCurrentTag(tag) {
             this.currentTag = tag
         },
