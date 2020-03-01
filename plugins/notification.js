d043a2db194a6af2ef273649cef39bfb12611dc6
Pythux <pythux@nowhere.org>
2020-03-01 19:21:07 +0100

    can add fa icon to miniToastr

--- plugins/notification.js
+++ plugins/notification.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import VueNotifications from 'vue-notifications'
 import miniToastr from 'mini-toastr'// https://github.com/se-panfilov/mini-toastr
 
+// You can use any font icon
+// miniToastr.setIcon('error', 'i', { class: 'fa fa-warning' })
+// miniToastr.setIcon('info', 'i', { class: 'fa fa-info-circle' })
+// miniToastr.setIcon('success', 'i', { class: 'fa fa-check-circle-o' })
+
 miniToastr.init()
 
 function toast({ title, message, type, timeout, cb }) {
